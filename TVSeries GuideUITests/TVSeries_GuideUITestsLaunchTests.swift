d7383779ea8942cff832858053fb065a60d46d54
David Zampier <davidzampier@gmail.com>
2022-02-02 18:21:09 -0300

    Initial Commit

---
+++ TVSeries GuideUITests/TVSeries_GuideUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TVSeries_GuideUITestsLaunchTests.swift
+//  TVSeries GuideUITests
+//
+//  Created by David Zampier on 02/02/22.
+//
+
+import XCTest
+
+class TVSeries_GuideUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
