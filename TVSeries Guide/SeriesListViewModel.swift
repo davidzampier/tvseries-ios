cf64dfcd697600df46e809c209c21006b4a4238a
David Zampier <davidzampier@gmail.com>
2022-02-03 18:50:19 -0300

    Creates the loading view

--- TVSeries Guide/SeriesListViewModel.swift
+++ TVSeries Guide/SeriesListViewModel.swift
@@ -7,31 +7,77 @@
 
 import UIKit
 
+protocol SeriesListViewModelDelegate: AnyObject {
+    func didUpdateResults()
+    func setLoading(isLoading: Bool)
+}
+
 final class SeriesListViewModel {
     
     private let seriesAPI: SeriesAPIProtocol
     
-    private(set) var series: [SeriesModel] = []
+    private var series: [SeriesModel] = []
+    private var searchedSeries: [SeriesModel]?
     
-    var didUpdateViewModel: (() -> Void)?
+    var isLoading: Bool = false {
+        willSet {
+            guard newValue != isLoading else { return }
+            self.delegate?.setLoading(isLoading: newValue)
+        }
+    }
+    
+    weak var delegate: SeriesListViewModelDelegate?
     
     init(seriesAPI: SeriesAPIProtocol = SeriesAPI()) {
         self.seriesAPI = seriesAPI
     }
     
+    
+    // MARK: - Table View Setup
+    
+    func numberOfItems() -> Int {
+        self.searchedSeries?.count ?? self.series.count
+    }
+    
+    func itemFor(indexPath: IndexPath) -> SeriesModel {
+        self.searchedSeries?[indexPath.row] ?? self.series[indexPath.row]
+    }
+    
+    
+    // MARK: - Search
+    
+    func didSearch(text: String?) {
+        let text = text ?? ""
+        guard !text.isEmpty else {
+            self.isLoading = false
+            self.searchedSeries = nil
+            self.delegate?.didUpdateResults()
+            return
+        }
+        self.isLoading = true
+        
+        if self.searchedSeries == nil {
+            self.searchedSeries = []
+        }
+        self.delegate?.didUpdateResults()
+    }
+    
+    
+    // MARK: - Fetch Data
+    
     func fetchSeries() {
-        self.seriesAPI.fetchSeries(page: nil) { (result: Result<[SeriesResponse], NetworkError>) in
+        self.seriesAPI.fetchSeries(page: nil) { [weak self] (result: Result<[SeriesResponse], NetworkError>) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let response):
                     let entities = response.map {
                         SeriesModel(id: $0.id, name: $0.name, summary: $0.summary, imageURL: $0.image.medium)
                     }
-                    self.series.append(contentsOf: entities)
+                    self?.series.append(contentsOf: entities)
                 case .failure:
                     break
                 }
-                self.didUpdateViewModel?()
+                self?.delegate?.didUpdateResults()
             }
         }
     }
--- TVSeries Guide/SeriesViewController.swift
+++ TVSeries Guide/SeriesViewController.swift
@@ -9,25 +9,64 @@ import UIKit
 
 class SeriesViewController: UITableViewController {
 
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var backgroundLabel: UILabel!
+    
+    private lazy var searchController: UISearchController = {
+        let controller = UISearchController()
+        controller.searchResultsUpdater = self
+        controller.obscuresBackgroundDuringPresentation = false
+        controller.searchBar.placeholder = "Type a series name"
+        return controller
+    }()
+    
     private let viewModel = SeriesListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.searchController = self.searchController
+        self.tableView.backgroundView = UIView()
+        self.viewModel.delegate = self
         self.viewModel.fetchSeries()
-        self.viewModel.didUpdateViewModel = { [weak self] in
-            self?.tableView.reloadData()
-        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setupBackgroundView()
+    }
+    
+    private func setupBackgroundView() {
+        guard self.backgroundView.superview == nil else { return }
+        self.backgroundView.frame = self.tableView.frame
+        self.tableView.backgroundView = self.backgroundView
+    }
+    
+    private func startLoading() {
+        self.backgroundLabel.text = "Searching...🕵️‍♂️"
+        self.backgroundLabel.isHidden = false
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+    }
+    
+    private func stopLoading() {
+        self.backgroundLabel.isHidden = true
+        self.activityIndicator.isHidden = true
+        self.activityIndicator.stopAnimating()
+    }
+    
+    
+    // MARK: - TableView Setup
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        self.viewModel.series.count
+        self.viewModel.numberOfItems()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SeriesListViewCell.identifier, for: indexPath) as? SeriesListViewCell else {
             return UITableViewCell()
         }
-        let series = self.viewModel.series[indexPath.row]
+        let series = self.viewModel.itemFor(indexPath: indexPath)
         cell.nameLabel.text = series.name
         cell.posterImageView.startLoading()
         self.viewModel.fetchPosterFor(series: series) { image in
@@ -39,5 +78,34 @@ class SeriesViewController: UITableViewController {
             }
         }
         return cell
+    }
+}
+
+
+// MARK: - SeriesListViewModelDelegate
+
+extension SeriesViewController: SeriesListViewModelDelegate {
+    
+    func setLoading(isLoading: Bool) {
+        if isLoading {
+            self.startLoading()
+        } else {
+            self.stopLoading()
+        }
+    }
+    
+    func didUpdateResults() {
+        self.tableView.reloadData()
+    }
+}
+
+
+// MARK: - UISearchResultsUpdating
+
+extension SeriesViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let text = searchController.searchBar.text
+        self.viewModel.didSearch(text: text)
     }
 }
