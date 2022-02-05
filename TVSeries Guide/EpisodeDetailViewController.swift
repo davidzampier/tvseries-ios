295bdce2224dddd82533041adce8c0c9336c8d2c
David Zampier <davidzampier@gmail.com>
2022-02-04 22:14:20 -0300

    Creates the EpisodeDetail view

---
+++ TVSeries Guide/EpisodeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailViewController.swift
+//  TVSeries Guide
+//
+//  Created by David Zampier on 04/02/22.
+//
+
+import UIKit
+
+class EpisodeDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: NetworkImageView!
+    @IBOutlet weak var seasonLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    
+}
